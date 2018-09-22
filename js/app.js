4577c29de126c08c77211a80ce275d557736dc18
Eunice Baiden <ebaiden@techie.com>
2018-09-22 19:01:22 -0400

    Added bug and made it move

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+let game = true;
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -9,7 +9,13 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
+
+    this.x = 0;
+    this.y = 60;
+    this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
 };
 
 // Update the enemy's position, required method for game
@@ -24,6 +30,13 @@ Enemy.prototype.update = function(dt) {
         //Increment x by speed * dt
     //else
         //Reset position to start
+    if(this.x < this.step * 5) {
+        //move forward
+        //increment x by speed * dt
+        this.x += 200 *dt;
+    } else {
+        this.x = this.resetPos;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -66,20 +79,69 @@ class Hero {
 
     constructor(){  //position properties
        
-        this.x = 0;
-        this.y = 0;
+        
         this.sprite = 'images/char-cat-girl.png';
+        this.height = 75;
+        this.width = 65;
+        this.step = 101;;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = this.jump * 5;
+        this.x = this.startX;
+        this.y = this.startY;
     }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     }
+
+    //handleInput(direction){
+    handleInput(input){
+        const horizontal = 101;
+        const vertical = 83;
+
+        /*if(direction === 'left' && this.x - horizontal >= 0) {
+        this.x -= 100;
+    }else if (direction === 'right' && this.x + horizontal < ctx.canvas.width ){
+        this.x += 100;
+    }else if (direction === 'down' && this.y + vertical < ctx.canvas.height -200){
+        this.y -=83;
+    }else if (direction  === 'up' && this.y + vertical > 0 - player.height){
+        this.y -= 83;
+    }*/
+        
+        switch(input) {
+            case 'left':
+            if (this.x > 0) {
+                this.x -= this.step;
+            }
+            break;
+
+            case 'up':
+            if (this.y > 0) {
+            this.y -= this.jump;
+             }
+            break;
+
+            case 'right':
+            if (this.x < this.step * 4) {
+            this.x += this.step;
+                }
+            break;
+
+            case 'down':
+            if (this.y < this.jump * 5) {
+            this.y += this.jump;
+            }
+            break ;   
+        }
+    }
 }
 
 
 
-
+/*food*/
 
 
 // Now write your own player class
@@ -90,7 +152,7 @@ class Hero {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const player = new Hero();
+
 
 
 // This listens for key presses and sends the keys to your
@@ -106,4 +168,10 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+const player = new Hero(202, 400, 'images/char-cat-girl.png');
+const bugOne  = new Enemy(-101, 0);
+const bugTwo  = new Enemy(-101, 83);
+const bugThree  = new Enemy((-100*2.5), 3);
+const allEnemies = [];
+allEnemies.push(bugOne)
 
